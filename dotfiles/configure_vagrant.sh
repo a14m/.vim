a5365420b9986a9256bb2f67cc8a975369e66560
Ahmed Abdel Razzak <eng.abdelrazzak@gmail.com>
2013-09-10 10:59:27 +0000

    vagrant configuration script added some more

--- dotfiles/configure_vagrant.sh
+++ dotfiles/configure_vagrant.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-#updating system packages
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install git-core -y
-sudo apt-get install vim -y
+##updating system packages
+#sudo apt-get update
+#sudo apt-get upgrade -y
+#sudo apt-get install git-core -y
+#sudo apt-get install vim -y
 
-#cloning vim configs/git configs/bashrc configs and gitsubmodule
-git clone https://github.com/artmees/dotvim.git ~/.vim
-ln -s ~/.vim/dotfiles/vimrc ~/.vimrc
-ln -s ~/.vim/dotfiles/gitconfig ~/.gitconfig
-rm ~/.bashrc
-ln -s ~/.vim/dotfiles/vagrant_bashrc ~/.bashrc 
-mkdir /vagrant/www
-source ~/.bashrc
-cd ~/.vim/
-git submodule init
-git submodule update
-cd ~
-git clone https://github.com/artmees/gitsubmodule.git
-cd gitsubmodule
-sudo ./install
-cd ~
-sudo rm -r gitsubmodule
+##cloning vim configs/git configs/bashrc configs and gitsubmodule
+#git clone https://github.com/artmees/dotvim.git ~/.vim
+#ln -s ~/.vim/dotfiles/vimrc ~/.vimrc
+#ln -s ~/.vim/dotfiles/gitconfig ~/.gitconfig
+#rm ~/.bashrc
+#ln -s ~/.vim/dotfiles/vagrant_bashrc ~/.bashrc 
+#mkdir /vagrant/www
+#source ~/.bashrc
+#cd ~/.vim/
+#git submodule init
+#git submodule update
+#cd ~
+#git clone https://github.com/artmees/gitsubmodule.git
+#cd gitsubmodule
+#sudo ./install
+#cd ~
+#sudo rm -r gitsubmodule
 
-#add other setting ups here as we go
+##install mysql
+#sudo apt-get install mysql-server -y
+#sudo apt-get install php5-mysql -y
+
+##install python virtualenv and pip
+#mkdir ~/.virtualenv
+#sudo apt-get install python-pip -y
+#sudo pip install virtualenv
