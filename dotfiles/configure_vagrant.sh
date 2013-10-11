8c18cb86ac21409aeaf48106dc6d51da38578900
Ahmed Abdel Razzak <eng.abdelrazzak@gmail.com>
2013-10-11 04:00:30 +0000

    also adding the .profile file to dotfiles

--- dotfiles/configure_vagrant.sh
+++ dotfiles/configure_vagrant.sh
@@ -13,15 +13,23 @@
 ##cloning vim configs/git configs/bashrc configs and gitsubmodule
 #################################################################
 #git clone https://github.com/artmees/dotvim.git ~/.vim
+
 #ln -s ~/.vim/dotfiles/vimrc ~/.vimrc
 #ln -s ~/.vim/dotfiles/gitconfig ~/.gitconfig
+
 #rm ~/.bashrc
 #ln -s ~/.vim/dotfiles/vagrant_bashrc ~/.bashrc 
+
+#rm ~/.profile
+#ln -s ~/.vim/dotfiles/vagrant_profile ~/.profile 
+
 #mkdir /vagrant/www
 #source ~/.bashrc
+
 #cd ~/.vim/
 #git submodule init
 #git submodule update
+
 #cd ~
 #git clone https://github.com/artmees/gitsubmodule.git
 #cd gitsubmodule
