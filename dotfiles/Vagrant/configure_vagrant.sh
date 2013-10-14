d7d2e173a8ddb730bbafa03ae7eae2446403ac86
Ahmed Abdel Razzak <eng.abdelrazzak@gmail.com>
2013-10-14 05:39:12 +0200

    adding ruby configs and mogodb

--- dotfiles/Vagrant/configure_vagrant.sh
+++ dotfiles/Vagrant/configure_vagrant.sh
@@ -83,3 +83,12 @@
 #echo 'eval "$(rbenv init -)"'
 #echo 'export RBENV_VERSION=1.9.3-p0'
 #echo "#################################################"
+
+
+################################
+##install AskNative dependencies
+################################
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+#echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+#sudo apt-get update
+#sudo apt-get install mongodb-10gen -y
